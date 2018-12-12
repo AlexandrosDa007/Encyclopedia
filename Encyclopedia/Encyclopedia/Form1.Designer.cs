ad13b288c44e27c94b64ccb6ee12c4909d4a1896
Alex Daltsis <alexdaltsis@gmail.com>
2018-12-12 12:04:22 +0200

    Changed

--- Encyclopedia/Encyclopedia/Form1.Designer.cs
+++ Encyclopedia/Encyclopedia/Form1.Designer.cs
@@ -256,7 +256,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(175, 49);
+            this.button1.Location = new System.Drawing.Point(367, 66);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
