3cdc1b2ed8f01eaff2f66079695fa0382b14712a
Ραφαήλ Τριανταφυλλίδης <botazz133@gmail.com>
2018-12-12 11:03:12 +0200

    Initial commit

---
+++ Encyclopedia/Encyclopedia/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Encyclopedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fooDataSet = new Encyclopedia.fooDataSet();
+            this.lemmaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lemmaTableAdapter = new Encyclopedia.fooDataSetTableAdapters.LemmaTableAdapter();
+            this.tableAdapterManager = new Encyclopedia.fooDataSetTableAdapters.TableAdapterManager();
+            this.lemmaBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.lemmaBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.lemmaDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.fooDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lemmaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lemmaBindingNavigator)).BeginInit();
+            this.lemmaBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lemmaDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fooDataSet
+            // 
+            this.fooDataSet.DataSetName = "fooDataSet";
+            this.fooDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // lemmaBindingSource
+            // 
+            this.lemmaBindingSource.DataMember = "Lemma";
+            this.lemmaBindingSource.DataSource = this.fooDataSet;
+            // 
+            // lemmaTableAdapter
+            // 
+            this.lemmaTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.AccountTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CategoryTableAdapter = null;
+            this.tableAdapterManager.Contact_GroupTableAdapter = null;
+            this.tableAdapterManager.ContactTableAdapter = null;
+            this.tableAdapterManager.Edited_LemmaTableAdapter = null;
+            this.tableAdapterManager.Education_LevelTableAdapter = null;
+            this.tableAdapterManager.Favorite_LemmaTableAdapter = null;
+            this.tableAdapterManager.LemmaTableAdapter = this.lemmaTableAdapter;
+            this.tableAdapterManager.RoleTableAdapter = null;
+            this.tableAdapterManager.Shared_LemmaTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = Encyclopedia.fooDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // lemmaBindingNavigator
+            // 
+            this.lemmaBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.lemmaBindingNavigator.BindingSource = this.lemmaBindingSource;
+            this.lemmaBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.lemmaBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.lemmaBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.lemmaBindingNavigatorSaveItem});
+            this.lemmaBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.lemmaBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.lemmaBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.lemmaBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.lemmaBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.lemmaBindingNavigator.Name = "lemmaBindingNavigator";
+            this.lemmaBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.lemmaBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.lemmaBindingNavigator.TabIndex = 0;
+            this.lemmaBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // lemmaBindingNavigatorSaveItem
+            // 
+            this.lemmaBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.lemmaBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("lemmaBindingNavigatorSaveItem.Image")));
+            this.lemmaBindingNavigatorSaveItem.Name = "lemmaBindingNavigatorSaveItem";
+            this.lemmaBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.lemmaBindingNavigatorSaveItem.Text = "Save Data";
+            this.lemmaBindingNavigatorSaveItem.Click += new System.EventHandler(this.lemmaBindingNavigatorSaveItem_Click);
+            // 
+            // lemmaDataGridView
+            // 
+            this.lemmaDataGridView.AutoGenerateColumns = false;
+            this.lemmaDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lemmaDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewImageColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.lemmaDataGridView.DataSource = this.lemmaBindingSource;
+            this.lemmaDataGridView.Location = new System.Drawing.Point(189, 122);
+            this.lemmaDataGridView.Name = "lemmaDataGridView";
+            this.lemmaDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.lemmaDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "lemma_title";
+            this.dataGridViewTextBoxColumn1.HeaderText = "lemma_title";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewImageColumn1
+            // 
+            this.dataGridViewImageColumn1.DataPropertyName = "lemma_body";
+            this.dataGridViewImageColumn1.HeaderText = "lemma_body";
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "category_id";
+            this.dataGridViewTextBoxColumn2.HeaderText = "category_id";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.Controls.Add(this.lemmaDataGridView);
+            this.Controls.Add(this.lemmaBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fooDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lemmaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lemmaBindingNavigator)).EndInit();
+            this.lemmaBindingNavigator.ResumeLayout(false);
+            this.lemmaBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lemmaDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private fooDataSet fooDataSet;
+        private System.Windows.Forms.BindingSource lemmaBindingSource;
+        private fooDataSetTableAdapters.LemmaTableAdapter lemmaTableAdapter;
+        private fooDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator lemmaBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton lemmaBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView lemmaDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
+
