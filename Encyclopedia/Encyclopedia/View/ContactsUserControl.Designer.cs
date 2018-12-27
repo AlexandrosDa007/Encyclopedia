a2137dc6fd6ba78189f51b55170240408862a8df
Despina Athanasleri <athanasleri@gmail.com>
2018-12-27 20:32:42 +0200

    add ListView

--- Encyclopedia/Encyclopedia/View/ContactsUserControl.Designer.cs
+++ Encyclopedia/Encyclopedia/View/ContactsUserControl.Designer.cs
@@ -33,7 +33,12 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.contactsTabPage = new System.Windows.Forms.TabPage();
             this.groupTabPage = new System.Windows.Forms.TabPage();
+            this.createGroupButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
             this.tabControl1.SuspendLayout();
+            this.contactsTabPage.SuspendLayout();
+            this.groupTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // searchContactsTextBox
@@ -78,6 +83,7 @@
             // 
             // contactsTabPage
             // 
+            this.contactsTabPage.Controls.Add(this.listView1);
             this.contactsTabPage.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.contactsTabPage.Location = new System.Drawing.Point(4, 30);
             this.contactsTabPage.Name = "contactsTabPage";
@@ -89,6 +95,8 @@
             // 
             // groupTabPage
             // 
+            this.groupTabPage.Controls.Add(this.listView2);
+            this.groupTabPage.Controls.Add(this.createGroupButton);
             this.groupTabPage.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.groupTabPage.Location = new System.Drawing.Point(4, 30);
             this.groupTabPage.Name = "groupTabPage";
@@ -97,6 +105,36 @@
             this.groupTabPage.TabIndex = 1;
             this.groupTabPage.Text = "Groups";
             this.groupTabPage.UseVisualStyleBackColor = true;
+            // 
+            // createGroupButton
+            // 
+            this.createGroupButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.createGroupButton.AutoSize = true;
+            this.createGroupButton.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.createGroupButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.createGroupButton.Location = new System.Drawing.Point(15, 14);
+            this.createGroupButton.Margin = new System.Windows.Forms.Padding(5);
+            this.createGroupButton.Name = "createGroupButton";
+            this.createGroupButton.Size = new System.Drawing.Size(129, 30);
+            this.createGroupButton.TabIndex = 18;
+            this.createGroupButton.Text = "Create Group";
+            this.createGroupButton.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(6, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(957, 315);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(6, 52);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(957, 269);
+            this.listView2.TabIndex = 19;
+            this.listView2.UseCompatibleStateImageBehavior = false;
             // 
             // ContactsUserControl
             // 
@@ -108,6 +146,9 @@
             this.Name = "ContactsUserControl";
             this.Size = new System.Drawing.Size(1036, 498);
             this.tabControl1.ResumeLayout(false);
+            this.contactsTabPage.ResumeLayout(false);
+            this.groupTabPage.ResumeLayout(false);
+            this.groupTabPage.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,5 +161,8 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage contactsTabPage;
         private System.Windows.Forms.TabPage groupTabPage;
+        private System.Windows.Forms.Button createGroupButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
     }
 }
