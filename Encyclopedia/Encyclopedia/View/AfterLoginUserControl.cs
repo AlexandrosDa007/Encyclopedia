afb50bd1f04a31c99a464bd8e41181984598d3b6
Despina Athanasleri <athanasleri@gmail.com>
2018-12-27 18:49:02 +0200

    After Login

---
+++ Encyclopedia/Encyclopedia/View/AfterLoginUserControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encyclopedia.View
+{
+    public partial class AfterLoginUserControl : UserControl
+    {
+        public AfterLoginUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contactsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void editedButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void favouritesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void profileButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
