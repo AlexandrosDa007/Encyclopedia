fbe30e3aff135115f55fdb1c6cc9a39f4468c1ae
StTsk <stavroula4ever@gmail.com>
2018-12-29 20:02:58 +0200

    Image list in ListView test

--- Encyclopedia/Encyclopedia/View/ContactsUserControl.cs
+++ Encyclopedia/Encyclopedia/View/ContactsUserControl.cs
@@ -19,16 +19,28 @@ namespace Encyclopedia.View
         {
             get
             {
-
                 if (_instance == null)
                     _instance = new ContactsUserControl();
                 return _instance;
 
             }
         }
+
         public ContactsUserControl()
         {
             InitializeComponent();
+			/*
+			ImageList imageList = new ImageList();
+			Image[] images = new Image[3];
+			images[0] = Image.FromFile("D:\\Witcher Info\\Unit Icons\\lyria_overview1.jpg");
+			images[1] = Image.FromFile("D:\\Witcher Info\\Unit Icons\\knight.jpg");
+			images[2] = Image.FromFile("D:\\Witcher Info\\Unit Icons\\vicovaro_2.jpg");
+
+			imageList.Images.AddRange(images);
+
+			contactsListView.LargeImageList = imageList;
+			*/
+
         }
 
     }
