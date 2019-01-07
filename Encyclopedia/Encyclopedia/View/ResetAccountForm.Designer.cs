a7eabe1d5a9070f4d0314d3ae4d8888121730170
Despina Athanasleri <athanasleri@gmail.com>
2019-01-07 17:13:50 +0200

    Reset account form

---
+++ Encyclopedia/Encyclopedia/View/ResetAccountForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Encyclopedia.View
+{
+    partial class ResetAccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResetAccountForm));
+            this.closePictureBox = new System.Windows.Forms.PictureBox();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.closePictureBox)).BeginInit();
+            this.topPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // closePictureBox
+            // 
+            this.closePictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("closePictureBox.Image")));
+            this.closePictureBox.Location = new System.Drawing.Point(444, 4);
+            this.closePictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.closePictureBox.Name = "closePictureBox";
+            this.closePictureBox.Size = new System.Drawing.Size(45, 34);
+            this.closePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.closePictureBox.TabIndex = 0;
+            this.closePictureBox.TabStop = false;
+            // 
+            // topPanel
+            // 
+            this.topPanel.AutoSize = true;
+            this.topPanel.Controls.Add(this.closePictureBox);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(493, 42);
+            this.topPanel.TabIndex = 2;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.emailLabel.Location = new System.Drawing.Point(80, 122);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(70, 22);
+            this.emailLabel.TabIndex = 24;
+            this.emailLabel.Text = "Email *";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.usernameLabel.Location = new System.Drawing.Point(35, 164);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(115, 22);
+            this.usernameLabel.TabIndex = 23;
+            this.usernameLabel.Text = "Username *";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.usernameTextBox.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(170, 164);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.usernameTextBox.MaxLength = 40;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(200, 28);
+            this.usernameTextBox.TabIndex = 22;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.emailTextBox.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextBox.Location = new System.Drawing.Point(170, 119);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.emailTextBox.MaxLength = 60;
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(200, 28);
+            this.emailTextBox.TabIndex = 21;
+            // 
+            // label
+            // 
+            this.label.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label.Location = new System.Drawing.Point(11, 46);
+            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(444, 53);
+            this.label.TabIndex = 29;
+            this.label.Text = "Please enter your email and username to reset your password";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // resetButton
+            // 
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.resetButton.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.resetButton.Location = new System.Drawing.Point(357, 210);
+            this.resetButton.Margin = new System.Windows.Forms.Padding(2);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(98, 32);
+            this.resetButton.TabIndex = 30;
+            this.resetButton.Text = "Reset password";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // ResetAccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(493, 262);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.topPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ResetAccountForm";
+            this.Text = "ResetAccountForm";
+            ((System.ComponentModel.ISupportInitialize)(this.closePictureBox)).EndInit();
+            this.topPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox closePictureBox;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
